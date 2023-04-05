782c577916e86e3689c89571c3e13c7209cafc7f
hermandi21 <121821476+hermandi21@users.noreply.github.com>
2023-04-05 13:17:40 +0200

    Code review verbesserungen

--- src/main/scala/de/htwg/se/reversi/model/Field.scala
+++ src/main/scala/de/htwg/se/reversi/model/Field.scala
@@ -3,16 +3,16 @@ package de.htwg.se.reversi.model
 case class Field(matrix : Matrix[Stone]):
     def this(size: Int, filling: Stone) = this(new Matrix(size, filling))
 
-    val size = matrix.size
-    val.eol = sys.props("line.separator")
+    def size = matrix.size
+    val eol = sys.props("line.separator")
 
-    def bar(cellWidth: Int = 8 , cellNum: Int = 8): String = (("+" + "-" * cellWidth) * cellNum) + "+" eol
+    def bar(cellWidth: Int = size , cellNum: Int = size): String = (("+" + "-" * cellWidth) * cellNum) + "+" eol
 
-    def cells(row: Int, cellWidth: Int = 8): String = 
+    def cells(row: Int, cellWidth: Int = size): String = 
         matrix.row(row).map(_.toString).map(" " * ((cellWidth - 1) / 2) + _ + " " * ((cellWidth - 1) / 2)).mkString("|", "|", "|") + eol
 
     //mkString( start: String, sep: String, end: String)
-    def mesh(cellWidth: Int = 8): String = 
+    def mesh(cellWidth: Int = size): String = 
         (0 until size).map(cells(_, cellWidth)).mkString(bar(cellWidth, size), bar(cellWidth, size), bar(cellWidth, size)) 
     override def toString = mesh()
 
