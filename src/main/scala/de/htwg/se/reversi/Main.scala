554a4f89d5f868f548ab30ac10b8c4aebd52e5de
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-04-19 19:26:29 +0200

    user input

--- src/main/scala/de/htwg/se/reversi/Main.scala
+++ src/main/scala/de/htwg/se/reversi/Main.scala
@@ -2,7 +2,30 @@ package de.htwg.se.reversi
 
 import de.htwg.se.reversi.model.{Field, Stone}
 
-@main def main(): Unit =
-  val field = Field().put(5,4,Stone.White)
-  println(field.display)
+import scala.io.StdIn.{readLine, readf, readf3}
 
+@main def main(): Unit =
+  var field = Field().put(5, 4, Stone.White)
+  var continue = true
+
+  while continue do {
+    println(field.display)
+    print("player> ")
+    val input = readLine()
+    input match {
+      case "q" => continue = false
+      case _ =>
+        val chars = input.toCharArray
+        val stone = chars(0) match
+          case 'X' => Stone.Black
+          case 'x' => Stone.Black
+          case 'O' => Stone.White
+          case 'o' => Stone.White
+          case _ => Stone.Nothing
+
+        val x = chars(1).toString.toInt
+        val y = chars(2).toString.toInt
+
+        field = field.put(y, x, stone)
+    }
+  }
