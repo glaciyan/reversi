4e08234ff3b00f51aee267c94d83307ec06b5722
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-04-26 09:20:41 +0200

    support for numbered stones

--- src/main/scala/de/htwg/se/reversi/model/Stone.scala
+++ src/main/scala/de/htwg/se/reversi/model/Stone.scala
@@ -7,3 +7,18 @@ enum Stone(view: Option[String]) {
   case Black extends Stone(Some("⚫"))
   case Nothing extends Stone(None)
 }
+
+object Stone {
+  def getNumber(n: Int): Option[String] = n match {
+    case 0 => Some("0️⃣")
+    case 1 => Some("1️⃣")
+    case 2 => Some("2️⃣")
+    case 3 => Some("3️⃣")
+    case 4 => Some("4️⃣")
+    case 5 => Some("5️⃣")
+    case 6 => Some("6️⃣")
+    case 7 => Some("7️⃣")
+    case 8 => Some("8️⃣")
+    case _ => None
+  }
+}
--- src/test/scala/de/htwg/se/reversi/model/StoneSpec.scala
+++ src/test/scala/de/htwg/se/reversi/model/StoneSpec.scala
@@ -21,4 +21,20 @@ class StoneSpec extends AnyWordSpec {
       }
     }
   }
+  "Number Stone" when {
+    "created" should {
+      "work" in {
+        Stone.getNumber(0).get should be("0️⃣")
+        Stone.getNumber(1).get should be("1️⃣")
+        Stone.getNumber(2).get should be("2️⃣")
+        Stone.getNumber(3).get should be("3️⃣")
+        Stone.getNumber(4).get should be("4️⃣")
+        Stone.getNumber(5).get should be("5️⃣")
+        Stone.getNumber(6).get should be("6️⃣")
+        Stone.getNumber(7).get should be("7️⃣")
+        Stone.getNumber(8).get should be("8️⃣")
+        Stone.getNumber(123) should be(None)
+      }
+    }
+  }
 }
