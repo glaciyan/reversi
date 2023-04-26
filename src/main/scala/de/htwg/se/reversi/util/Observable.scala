e369d0c772f7db6446da5e3a687a8a051384923c
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-04-26 09:20:47 +0200

    observer

---
+++ src/main/scala/de/htwg/se/reversi/util/Observable.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.reversi.util
+
+trait Observable {
+  private var listeners: Vector[Observer] = Vector()
+
+  def add(o: Observer) = listeners = listeners :+ o
+
+  def remove(o: Observer) = listeners = listeners.filterNot(_ == o)
+
+  def notifyObservers(): Unit = listeners.foreach(_.update())
+}
