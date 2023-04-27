31fe1add72311062c242630505f3ec720d5ebc35
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-04-27 15:17:27 +0200

    add method to get a single stone from field

--- src/main/scala/de/htwg/se/reversi/model/Field.scala
+++ src/main/scala/de/htwg/se/reversi/model/Field.scala
@@ -9,5 +9,7 @@ case class Field(m: Matrix[Stone] = new Matrix(8, Stone.Nothing)) {
 
   def put(row: Int, col: Int, stone: Stone): Field = copy(m.putCell(row, col, stone))
 
+  def getStone(row: Int, col: Int): Stone = m.cell(row, col)
+
   def display: String = (0 until size).map(row).mkString(eol)
 }
