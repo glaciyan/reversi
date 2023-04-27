9b0e2eb7621e1a9e09af1fd932f54115cc293a80
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-04-27 15:17:56 +0200

    new tui using events

--- src/main/scala/de/htwg/se/reversi/views/TUIView.scala
+++ src/main/scala/de/htwg/se/reversi/views/TUIView.scala
@@ -1,8 +1,11 @@
 package de.htwg.se.reversi.views
 
 import de.htwg.se.reversi.controller.GameState
-import de.htwg.se.reversi.util.Observer
+import de.htwg.se.reversi.util.Event.{AlreadyPlacedError, GameDone, Placed}
+import de.htwg.se.reversi.util.{Event, Observer}
 
+import java.text.ParseException
+import java.util.{InputMismatchException, Scanner}
 import scala.io.StdIn
 
 class TUIView(state: GameState) extends GameUI, Observer {
@@ -11,15 +14,36 @@ class TUIView(state: GameState) extends GameUI, Observer {
   override def run(): Unit = inputLoop()
 
   private def inputLoop(): Unit = {
+    println(state.field.display)
     while !state.finished do {
-      println(state.field.display)
-      waitForInput()
+      print(s"${state.currentPlayer} > ")
+      val input = waitForInput()
+      input match {
+        case Some((row, col)) => state.put(row, col)
+        case None => println("Invalid input please try again")
+      }
     }
   }
 
-  private def waitForInput(): Unit = {
-    StdIn.readLine()
+  private def waitForInput(): Option[(Int, Int)] = {
+    try {
+      val input = StdIn.readLine()
+      val scanner = new Scanner(input)
+      val values: (AnyVal, AnyVal) = (scanner.nextInt(), scanner.nextInt())
+      scanner.close()
+
+      values match {
+        case (row: Int, col: Int) if row < state.field.size => Some((row, col))
+        case _ => None
+      }
+    } catch {
+      case _: Throwable => None
+    }
   }
 
-  override def update(): Unit = ???
+  override def update(e: Event): Unit = e match {
+    case Placed => println(state.field.display)
+    case AlreadyPlacedError => println("You can't replace other stones")
+    case GameDone =>
+  }
 }
