c2c7b6dc5b9298be875f1b34294d103ba07de00a
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-05-18 15:16:19 +0200

    use default value

--- src/main/scala/de/htwg/se/reversi/Main.scala
+++ src/main/scala/de/htwg/se/reversi/Main.scala
@@ -10,7 +10,7 @@ import scala.io.StdIn.{readLine, readf, readf3}
 
 @main def main(): Unit =
   val field = Field().put(3, 3, Stone(BlackStone)).put(3, 4, Stone(WhiteStone)).put(4, 3, Stone(WhiteStone)).put(4, 4, Stone(BlackStone))
-  val state = Controller(field, WhiteStone, false)
+  val state = Controller(field, WhiteStone)
   val game: GameUI = TUIView(state) // Template Pattern
   game.run()
 
