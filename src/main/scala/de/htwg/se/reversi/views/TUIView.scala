df118da89aecbe2ce9103079b369693cd13bc2ea
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-05-25 11:00:37 +0200

    properly use try instead of option

--- src/main/scala/de/htwg/se/reversi/views/TUIView.scala
+++ src/main/scala/de/htwg/se/reversi/views/TUIView.scala
@@ -3,11 +3,12 @@ package de.htwg.se.reversi.views
 import de.htwg.se.reversi.controller.Controller
 import de.htwg.se.reversi.model.Field
 import de.htwg.se.reversi.util.PutEvent.{AlreadyPlacedError, GameDone, Placed}
-import de.htwg.se.reversi.util.{PutEvent, Observer}
+import de.htwg.se.reversi.util.{Observer, PutEvent}
 
 import java.text.ParseException
 import java.util.{InputMismatchException, Scanner}
 import scala.io.StdIn
+import scala.util.{Failure, Success, Try}
 
 class TUIView(controller: Controller) extends GameUI, Observer {
   controller.add(this)
@@ -20,14 +21,14 @@ class TUIView(controller: Controller) extends GameUI, Observer {
       print(s"${controller.currentPlayer.renderText()} > ")
       val input = readInput()
       input match {
-        case Some((row, col)) => controller.put(row, col)
-        case None => println("Invalid input please try again")
+        case Success((row, col)) => controller.put(row, col)
+        case Failure(_) => println("Invalid input please try again")
       }
     }
   }
 
   // TODO: testen
-  private def readInput(): Option[(Int, Int)] = {
+  private def readInput(): Try[(Int, Int)] = {
     try {
       val input = StdIn.readLine()
       val scanner = new Scanner(input)
@@ -35,11 +36,11 @@ class TUIView(controller: Controller) extends GameUI, Observer {
       scanner.close()
 
       values match {
-        case (row: Int, col: Int) if row < controller.field.size => Some((row, col))
-        case _ => None
+        case (row: Int, col: Int) if row < controller.field.size => Success((row, col))
+        case _ => Failure(new InputMismatchException())
       }
     } catch {
-      case _: Throwable => None
+      case e: Throwable => Failure(e)
     }
   }
 
