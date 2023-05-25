c551294cd6381823ea0fab1b6f14455224946ec8
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-05-25 11:52:28 +0200

    use checked

--- src/main/scala/de/htwg/se/reversi/Main.scala
+++ src/main/scala/de/htwg/se/reversi/Main.scala
@@ -1,16 +1,16 @@
 // $COVERAGE-OFF$Disabling highlighting by default until a workaround for https://issues.scala-lang.org/browse/SI-8596 is found
 package de.htwg.se.reversi
 
-import de.htwg.se.reversi.controller.GameController
+import de.htwg.se.reversi.controller.{CheckedGameController, GameController}
 import de.htwg.se.reversi.model.Field
-import de.htwg.se.reversi.model.stone.{BlackStone, WhiteStone, Stone, StoneState}
+import de.htwg.se.reversi.model.stone.{BlackStone, Stone, StoneState, WhiteStone}
 import de.htwg.se.reversi.views.{GameUI, TUIView}
 
 import scala.io.StdIn.{readLine, readf, readf3}
 
 @main def main(): Unit =
   val field = Field().put(3, 3, Stone(BlackStone)).put(3, 4, Stone(WhiteStone)).put(4, 3, Stone(WhiteStone)).put(4, 4, Stone(BlackStone))
-  val state = GameController(field, WhiteStone)
+  val state = CheckedGameController(GameController(field, WhiteStone))
   val game: GameUI = TUIView(state) // Template Pattern
   game.run()
 
