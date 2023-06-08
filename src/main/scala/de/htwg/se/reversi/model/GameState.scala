ba82c5db1353d84f8b8ec25940b2e66eb4efdb7e
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-06-08 15:28:02 +0200

    remove failing import

--- src/main/scala/de/htwg/se/reversi/model/GameState.scala
+++ src/main/scala/de/htwg/se/reversi/model/GameState.scala
@@ -2,7 +2,6 @@ package de.htwg.se.reversi.model
 
 import de.htwg.se.reversi.model.stone.{BlackStone, NoStone, Stone, StoneState, WhiteStone}
 import de.htwg.se.reversi.util.PutEvent
-import de.htwg.se.reversi.util.PutEvent.{AlreadyPlacedError, Placed}
 
 case class GameState(field: Field, currentPlayer: StoneState) {
   def put(row: Int, col: Int): GameState = this.put(row, col, currentPlayer, nextPlayer)
