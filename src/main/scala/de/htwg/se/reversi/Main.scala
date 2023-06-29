8934d7dd74493a881dc9a09a6ab89802b6fec760
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-06-29 08:52:32 +0200

    optimize imports

--- src/main/scala/de/htwg/se/reversi/Main.scala
+++ src/main/scala/de/htwg/se/reversi/Main.scala
@@ -2,12 +2,7 @@
 package de.htwg.se.reversi
 
 import de.htwg.se.reversi.controller.modules.Default.given
-import de.htwg.se.reversi.controller.{Controller, IController}
-import de.htwg.se.reversi.model.Field
-import de.htwg.se.reversi.model.stone.{BlackStone, Stone, StoneState, WhiteStone}
-import de.htwg.se.reversi.views.{GUIView, GameUI, TUIView}
-
-import scala.io.StdIn.{readLine, readf, readf3}
+import de.htwg.se.reversi.views.{GUIView, GameUI}
 
 @main def main(): Unit =
   val game: GameUI = GUIView()
